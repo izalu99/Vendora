723d579c2dc62952292fbfa54629636df6148f3c
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-10-06 18:37:44 -0600

    authentication with jwt

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from userauths.models import User, Profile
+from userauths.serializer import RegisterSerializer, MyTokenObtainPairSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
